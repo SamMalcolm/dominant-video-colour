824a678cdac9c6643968a7b6e099b3fddc1809ef
SamMalcolm <sam_malcolm@live.com.au>
2019-08-09 16:46:25 +1000

    Ready 2 go

---
+++ index.js
@@ -0,0 +1,115 @@
+const ColorThief = require('colorthief');
+const extractFrames = require('ffmpeg-extract-frames');
+const _cliProgress = require('cli-progress');
+const fs = require('fs');
+const bar1 = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+// bar1.start(200, 0);
+// bar1.update(100);
+// bar1.stop();
+
+let colours = [];
+let fileName = process.argv[2];
+
+const processData = async (fileName) => {
+	return new Promise(async (resolve, reject) => {
+
+		await extractFrames({
+			input: fileName,
+			output: './output/frames/frame-%d.png'
+		}).catch((err) => { console.log(err) })
+
+		fs.readdir('./output/frames', async (err, files) => {
+			if (err) {
+				console.log("READ DIR ERROR");
+				console.log(err);
+			}
+			var attempts = 0;
+			for (let i = 0; i < files.length; i++) {
+				console.log("EXISTS");
+				fs.exists('output/frames/' + files[i], async (exists) => {
+					if (exists) {
+						console.log("GET COLOUR");
+						let color = await ColorThief.getColor('output/frames/' + files[i])
+						attempts++;
+						console.log(attempts);
+						console.log(typeof color);
+						console.log(color);
+						if (typeof color == "object") {
+							console.log("ADDING COLOUR TO ARRAY");
+							console.log(color[0]);
+							console.log(color[1]);
+							console.log(color[2]);
+							let frameColour = {};
+							frameColour.index = files[i].indexOf('-');
+							frameColour.index = files[i].slice(frameColour.index + 1, files[i].indexOf('.'));
+							frameColour.index = parseInt(frameColour.index);
+							frameColour.colour = color;
+							colours.push(frameColour);
+							console.log("DELETING FILE");
+							fs.unlink('output/frames/' + files[i], (err) => {
+								console.log("VALUE OF I " + i);
+								console.log(colours.length + " / " + files.length);
+								if (attempts == (files.length - 1)) {
+									colours = colours.sort((a, b) => { return a.index - b.index })
+									resolve(colours);
+									fs.writeFileSync('output/json/colours.json', JSON.stringify(colours));
+								}
+							})
+
+						}
+					}
+				});
+			}
+			console.log(colours);
+		});
+
+	});
+}
+
+const generateMarkup = (coloursArr) => {
+	// calc(100% / ${coloursArr.length})
+	console.log("IN RESOLVE CALL BACK");
+	let colourMarkup = "";
+	console.log(coloursArr.length);
+	for (let i = 0; i < coloursArr.length; i++) {
+		colourMarkup += `
+		<div style="width:100%; background-color:rgb(${coloursArr[i].colour[0]},${coloursArr[i].colour[1]},${coloursArr[i].colour[2]});"></div>`;
+	}
+	fileName = fileName.split('/');
+	fileName = fileName[fileName.length - 1];
+	fileName = fileName.split('.')[0];
+	let markup = `
+	<html>
+		<head>
+			<title>${fileName}</title>
+		</head>
+		<body style="display:flex; flex-direction:row; position:absolute; margin:0; padding:0; width:100%; height:100%">
+		<div style="position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: stretch;
+		justify-items: stretch;">
+			${colourMarkup}
+			</div>
+		</body>
+	</html>
+	`;
+
+	fs.writeFile('output/html/' + fileName + '.html', markup, (err) => {
+		console.log(err);
+	})
+}
+
+// fs.readFile('output/json/colours.json', (err, data) => {
+// 	let colours = JSON.parse(data);
+// 	generateMarkup(colours);
+// })
+processData(fileName).then((coloursArr) => {
+
+	generateMarkup(coloursArr);
+
+});
